2557a27b5e7f9da238ed112f0a8c2ad11ab4b99d
corinaalarcon <corina.alarcon@gmail.com>
2020-07-06 14:30:10 -0300

    cambios para empleados

--- models/getlist.php
+++ models/getlist.php
@@ -3,82 +3,105 @@
   /*Definiciones de pseudo-tokens y conector a DB */
 
 require_once('dbconn.php');
-         //var_dump($_POST);
-         $token=sanitize($_POST['token']);
-         $busqueda=sanitize($_POST['busqueda']);
-         $tb=sanitize($_POST['tipobusqueda']);
+        //var_dump($_POST);
+        $token=sanitize($_POST['token']);
+        $busqueda=sanitize($_POST['busqueda']);
+        $tb=sanitize($_POST['tipobusqueda']);
         $donde='';
 
-$localidad=" LEFT JOIN localidad ON empresa.id_localidad = localidad.id_localidad  ";
+
 
 if (!empty($busqueda)){
- switch ($tb) {
-     case 'CUIT':
-          $donde=" WHERE cuit LIKE '%".$busqueda."%'";
 
-       break;
-     case 'RS':
-          $donde=" WHERE razon_social LIKE '%".$busqueda."%'";
+switch ($tb) {
+    case 'CUIT':
+        $donde=" WHERE cuit LIKE '%".$busqueda."%' AND";
 
-       break;
-     default:
-         $donde = '';      
+       	break;
+    case 'RS':
+        $donde=" WHERE razon_social LIKE '%".$busqueda."%' AND";
 
-       break;
+       	break;
+	case 'DNICUIL':
+		$donde="AND fecha_baja IS NULL AND (documento LIKE '%".$busqueda."%' OR cuil LIKE '%".$busqueda."%')"; 
+		
+	   	break;
+	case 'AN':
+		$donde="AND fecha_baja IS NULL AND (apellido LIKE '%".$busqueda."%' OR nombre LIKE '%".$busqueda."%')";
+
+		break;
+    default:
+        $donde = " ";      
+
+      	break;
    }
           
 }
 
-   switch ($token) {
-     case empresas:
-          $tabla='empresa';
-          $idtabla = 'id_empresa';
-       break;
+
+	switch ($token) {
+		case empresas:
+			$tabla='empresa empr';
+			$idtabla = 'id_empresa';
+			$localidad = ", localidad lo WHERE empr.id_localidad = lo.id_localidad AND flg_baja=0 AND";
+		break;
      
-     case empleados:
-          $tabla='empleado';
-          $idtabla = ' id_empleado ';
-       break;
+		case empleados:
+			$tabla='empleado empl';
+			$idtabla = 'id_empleado';
+			$localidad = ", localidad lo WHERE empl.id_localidad = lo.id_localidad AND flg_baja=0 ";
+		break;
+
+		default:
+			die(json_encode(array('error' => 'Sin identificador de tablas.')));      
+		break;
+	}
 
 
-
-     default:
-         die(json_encode(array('error' => 'Sin identificador de tablas.')));      
-       break;
-   }
-
-    $consulta =  "SELECT * FROM `".$tabla."`  ".$donde." ORDER BY ".$idtabla;
-    $query = $dbconn->prepare(" SELECT * FROM `".$tabla."`  ".$localidad."  ".$donde." ORDER BY localidad");
+    $consulta =  "SELECT * FROM ".$tabla. $localidad .$donde." ORDER BY ".$idtabla;
+    $query = $dbconn->prepare($consulta);
     $query->execute();
     $parse="";
 
-    //var_dump($consulta);
 
-   switch ($token) {
+if($query->rowCount() < 1){
+	echo 'Sin resultados';
+}
+else{
+	switch ($token) {
 
-     case empresas:
-/*
-'id_empresa' ,  'razon_social' 'cuit' 'correo_electronico' 'domicilio' 'telefono' 'fecha_inicio_actividad' 'id_localidad' 'id_rama_empresa' 'id_usuario_sistema' 'fecha_alta' 'fecha_baja' 'flg_baja'*/
-		    while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-		      $parse = $parse.'<tr>
-          <td>'.$row['cuit']. '</td>
-          <td>'. $row['razon_social']. '</td>
-          <td>'. $row['telefono']. '</td>
-          <td>'. $row['domicilio']. '</td>
-          <td>'. $row['localidad']. '</td>
-          <td>'. $row['fecha_alta']. '</td>
-          <td><div class="td-actions"><a href="./?acc=empresasficha&id='.$row['id_empresa'].'" class="icon red" data-toggle="tooltip" data-placement="top" title="Ampliar"><i class="icon-circle-with-plus"></i></a><a href="./index.php?acc=empresas&ver='.$row['id_empresa'].'" class="icon green" data-toggle="tooltip" data-placement="top" title="Ver/Modificar"><i class="icon-save"></i></a><a href="#" class="icon blue" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="icon-cancel"></i></a></div></td></tr>';
+		case empresas:
+		/*
+		'id_empresa' ,  'razon_social' 'cuit' 'correo_electronico' 'domicilio' 'telefono' 'fecha_inicio_actividad' 'id_localidad' 'id_rama_empresa' 'id_usuario_sistema' 'fecha_alta' 'fecha_baja' 'flg_baja'*/
+			while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+				$parse = $parse.'<tr>
+								  <td>'.$row['cuit']. '</td>
+								  <td>'. $row['razon_social']. '</td>
+								  <td>'. $row['telefono']. '</td>
+								  <td>'. $row['domicilio']. '</td>
+								  <td>'. $row['id_localidad']. '</td>
+								  <td>'. $row['fecha_alta']. '</td>
+								  <td><div class="td-actions"><a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Agregar"><i class="icon-circle-with-plus"></i></a><a href="./index.php?acc=empresas&ver='.$row['id_empresa'].'" class="icon green" data-toggle="tooltip" data-placement="top" title="Ver/Modificar"><i class="icon-save"></i></a><a href="#" class="icon blue" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="icon-cancel"></i></a></div></td></tr>';
 		    }
-			 echo $parse;							
+			
+			echo $parse;	
+									
        break;     
 
      case empleados:
 		    while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-		      $parse = $parse.'<tr><td>'.$row['id_categoria']. '</td><td>'. $row['nombre_categoria']. '</td><td><div class="td-actions"><a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Agregar"><i class="icon-circle-with-plus"></i></a><a href="#" class="icon green" data-toggle="tooltip" data-placement="top" title="Modificar"><i class="icon-save"></i></a><a href="#" class="icon blue" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="icon-cancel"></i></a></div></td></tr>';
+				$parse = $parse.'<tr>
+								  <td>'.$row['documento']. '</td>
+								  <td>'.$row['cuil']. '</td>
+								  <td>'.$row['apellido']. '</td>
+							      <td>'.$row['nombre']. '</td>
+							      <td>'.$row['telefono']. '</td>
+							      <td>'.$row['domicilio']. '</td>
+							      <td>'.$row['localidad']. '</td>
+							      <td>'.$row['fecha_alta']. '</td>
+							      <td><div class="td-actions"><a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Agregar"><i class="icon-circle-with-plus"></i></a><a href="./index.php?acc=empleados&ver='.$row['id_empleado'].'" class="icon green" data-toggle="tooltip" data-placement="top" title="Ver/Modificar"><i class="icon-save"></i></a><a href="#" class="icon blue" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="icon-cancel"></i></a></div></td></tr>';
 		    }
-
-
-			var_dump($parse);							
+			echo $parse;						
        break; 
 
 
@@ -87,6 +110,6 @@ if (!empty($busqueda)){
          die(json_encode(array('error' => 'Sin identificador de tablas.')));      
        break;
    }
-
+}
 
     $dbconn = null;
